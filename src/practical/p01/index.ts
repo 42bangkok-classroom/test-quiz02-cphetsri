7e539de48a7ba959f60b1669f473dff77abce038
C Phetsri <cphetsr@RP3543w>
2026-01-16 19:39:12 +0700

    i've triedgit add .

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,26 @@
-export function getPostalAddress() {}
+import axios from 'axios';
+
+interface User {
+    id: number;
+    name: string;
+    phone: string;
+    address: string;
+
+}
+
+async function api() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "GET",
+        body: JSON.stringify({
+            id: ,
+            name: [],
+            phone: [],
+            address: [];
+        })
+    });
+    
+}
+
+export function getPostalAddress(id, name, phone, address) {
+    return 
+}
